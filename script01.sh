37cd388f750345eb5478742f165bb0b9a51557f3
bheema-tech <bheemesha590@gmail.com>
2025-05-29 18:45:48 +0530

    Roboshop-shell scirpts

--- script01.sh
+++ script01.sh
@@ -4,7 +4,7 @@ GREETING="Hello World Good morning"
 echo "GREETING$"
 echo "PID of Script1: $$"
 
-./script02.sh
+source ./script02.sh
 
-# when you try to execute the script first time, permission denied,we need to provide the permissions for 2nd script
+# when you try to execute the script first time, permission denied,we need to provide the permissions for 2nd script, if you add source infront of the line, permissions not required and same PID will be used.
 
