eb5c9b855c3364c3f1a387c20f8ee64421fee15d
bheema-tech <bheemesha590@gmail.com>
2025-05-29 21:29:46 +0530

    Roboshop-shell scirpts

--- common.sh
+++ common.sh
@@ -4,6 +4,7 @@ USERD=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/logs/roboshop_logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -27,9 +28,9 @@ check_root(){
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ...  $G  SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ----$R Failure $N" | tee -a $LOG_FILE
+        echo -e "$2 is ---- $R Failure $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
